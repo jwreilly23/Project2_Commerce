4815b15488e5fa6800109781d231f928f16e9fd5
Jack Reilly <jwreilly23@gmail.com>
2021-03-02 18:36:50 -0500

    Before adding categories into Django form

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,42 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    # make plural categories instead of categorys
+    class Meta:
+        verbose_name_plural = "categories"
+
+        
+class Listing(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=200)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    # optional inputs 
+    image = models.URLField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="listings", null=True, blank=True)
+
+    def __str__(self):
+        return f"Title: {self.listing}"
+
+
+class Watchlist(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="watchlists")
+    listing_id = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="watchlists")
+
+    def __str__(self):
+        return f"User {self.user_id} is watching listing {self.listing_id}"
+
+
+class Bids(models.Model):
+    bidder_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bidss")
+    listing_id = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"User {self.bidder_id} bid {self.amount} on listing {self.listing_id}"
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -3,8 +3,23 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
 
-from .models import User
+from .models import User, Category, Listing, Watchlist
+
+
+class ListingForm(forms.Form):
+    title = forms.CharField(label="Title", max_length=30)
+    description = forms.CharField(label="Description", max_length=200)
+    image = forms.URLField(label="Image (optional)", required=False)
+
+    # create tuple for category choices
+    choices = ()
+    for category in Category.objects.all():
+        choices = choices + (category, category.name)
+    category = forms.ChoiceField(choices=choices)
+
+    
 
 
 def index(request):
@@ -61,3 +76,40 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+def new_listing(request):    
+    if request.method == "POST":
+        # take the posted data and save as form
+        form = ListingForm(request.POST)
+
+        # confirm form data is valid
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            description = form.cleaned_data["description"]
+            image = form.cleaned_data["image"]
+        
+        # if form data was not valid, re-render page with existing information
+        else: 
+            return render(request, "auctions/new_listing.html", {
+            "form": form
+        })
+
+        # log the new listing in Listing model
+        # new_listing = Listing(title=title, description=description, image=image, category=category)
+        # new_listing.save()
+
+
+        # TEMP - RETURN FORM INFO
+        return render(request, "auctions/temp.html", {
+            "title": title,
+            "description": description,
+            "image": image,
+            "category": category
+        })
+
+    else:
+        return render(request, "auctions/new_listing.html", {
+            "form": ListingForm(),
+            "categories": Category.objects.all()
+        })
