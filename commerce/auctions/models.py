203d99c3cc1bb51b8e44d61bd7ce2b709e423e4b
Jack Reilly <jwreilly23@gmail.com>
2021-03-04 20:13:12 -0500

    Listing pages updates

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -20,12 +20,17 @@ class Listing(models.Model):
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=200)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    open_status = models.BooleanField(default=True)
     # optional inputs 
     image = models.URLField(null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="listings", null=True, blank=True)
-
+    
+    if open_status:
+        status = "OPEN"
+    else:
+        status = "CLOSED"
     def __str__(self):
-        return f"Title: {self.listing}"
+        return f"Title: {self.title}---Owner: {self.owner.username}---{self.status}"
 
 
 class Watchlist(models.Model):
@@ -36,10 +41,10 @@ class Watchlist(models.Model):
         return f"User {self.user_id} is watching listing {self.listing_id}"
 
 
-class Bids(models.Model):
-    bidder_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bidss")
+class Bid(models.Model):
+    bidder_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
     listing_id = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
     amount = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
-        return f"User {self.bidder_id} bid {self.amount} on listing {self.listing_id}"
+        return f"User {self.bidder_id} bid {self.amount} on listing {self.listing_id.title}"
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -4,26 +4,49 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django import forms
+from django.db.models import Max
+from django.utils.translation import gettext_lazy as _
 
-from .models import User, Category, Listing, Watchlist
+from .models import User, Category, Listing, Watchlist, Bid
 
 
-class ListingForm(forms.Form):
-    title = forms.CharField(label="Title", max_length=30)
-    description = forms.CharField(label="Description", max_length=200)
-    image = forms.URLField(label="Image (optional)", required=False)
-
+# class ListingForm(forms.Form):
+#     title = forms.CharField(label="Title", max_length=30)
+#     description = forms.CharField(label="Description", max_length=200)
+#     image = forms.URLField(label="Image (optional)", required=False)
+#     temp = forms.DecimalField()
     # create tuple for category choices
     # choices = []
     # for category in Category.objects.all():
     #     choices = choices + [category.id, category.name]
     # category = forms.ChoiceField(choices=choices)
 
+class ListingForm(forms.ModelForm):
+    class Meta:
+        model = Listing
+        fields = ("title", "description", "image", "category")
+
+
+class BidForm(forms.Form):
+    starting_bid = forms.DecimalField(label="Starting Bid:", max_value=9999.99, min_value=1.00, decimal_places=2)
     
 
-
 def index(request):
-    return render(request, "auctions/index.html")
+    # get all active listings
+    active_listings = Listing.objects.filter(open_status=True)
+
+    # get bids for active listings
+    current_max_bids = []
+    for listing in active_listings:
+        # add the maximum bid to the list for each listing
+        current_max_bids.append( "%.2f" % Bid.objects.filter(listing_id = listing.id).aggregate(Max('amount'))['amount__max'] )
+
+    # combine lists to iterate concurrently; https://stackoverflow.com/questions/2415865/iterating-through-two-lists-in-django-templates
+    listings_and_bids = zip(active_listings, current_max_bids)
+
+    return render(request, "auctions/index.html", {
+        "listings_and_bids": listings_and_bids
+    })
 
 
 def login_view(request):
@@ -81,49 +104,70 @@ def register(request):
 def new_listing(request):
     if request.method == "POST":
         # take the posted data and save as form
-        form = ListingForm(request.POST)
+        listing_form = ListingForm(request.POST)
+        bid_form = BidForm(request.POST)
 
         # confirm form data is valid
-        if form.is_valid():
-            title = form.cleaned_data["title"]
-            description = form.cleaned_data["description"]
-            image = form.cleaned_data["image"]
+        if listing_form.is_valid() and bid_form.is_valid():
+            title = listing_form.cleaned_data["title"]
+            description = listing_form.cleaned_data["description"]
+            image = listing_form.cleaned_data["image"]
+            category = listing_form.cleaned_data["category"]
+            starting_bid = bid_form.cleaned_data["starting_bid"]
         
-        # if form data was not valid, re-render page with existing information
+        # if form data was not valid, re-render page
         else: 
             return render(request, "auctions/new_listing.html", {
-            "form": form
+            "listing_form": ListingForm(),
+            "bid_form": BidForm(),
+            "message": "Invalid inputs"
         })
 
         # check valid category
-        category = request.POST["category"]
-        try:
-            test_object = Category.objects.get(id=int(category))
-        except Category.DoesNotExist:
-            return render(request, "auctions/new_listing.html", {
-            "form": ListingForm(),
-            "categories": Category.objects.all(),
-            "message": "Invalid Category"
-        })
+        # category = request.POST["category"]
+        # try:
+        #     test_object = Category.objects.get(id=int(category))
+        # except Category.DoesNotExist:
+        #     return render(request, "auctions/new_listing.html", {
+        #     "form": ListingForm(),
+        #     "categories": Category.objects.all(),
+        #     "message": "Invalid Category"
+        # })
 
+        # insert new listing data into Listing
+        current_user = User.objects.get(id=request.user.id)
+        new_listing = Listing(title=title, description=description, image=image, category=category, owner=current_user)
+        new_listing.save()
+        # insert starting bid
+        starting_bid = Bid(listing_id=new_listing, bidder_id=current_user, amount=starting_bid)
+        starting_bid.save()
 
-        # log the new listing in Listing model
-        # new_listing = Listing(title=title, description=description, image=image, category=category)
-        # new_listing.save()
-
-
-        # TEMP - RETURN FORM INFO
-        
+        # TEMP - RETURN FORM INFO        
         return render(request, "auctions/temp.html", {
             "title": title,
             "description": description,
             "image": image,
             "category": category,
-            "test_object": test_object
+            "request.user.id": request.user.id
         })
 
     else:
         return render(request, "auctions/new_listing.html", {
-            "form": ListingForm(),
-            "categories": Category.objects.all()
+            "listing_form": ListingForm(),
+            "bid_form": BidForm()
         })
+
+
+def listing_page(request, listing_title, listing_id):
+    
+    listing = Listing.objects.get(id=listing_id)
+
+    context = {
+        "listing": listing
+    }
+    return render(request, "auctions/listing_page.html", context)
+
+
+def watchlist(request, username):
+    current_user = User.objects.get(id=request.user.id)
+    watchlist = Watchlist.objects.filter(user_id=current_user)
